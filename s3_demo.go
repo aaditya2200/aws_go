079790cb58b338e74efcad734310131736bab94a
Aaditya <aaditya2200@gmail.com>
2021-06-20 22:43:55 +0530

    Go file which retrieves contents of an S3 bucket

--- s3_demo.go
+++ s3_demo.go
@@ -6,27 +6,37 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"log"
+	"os"
 )
 
+/*
+	A simple demo of the AWS SDK for Golang,
+	Script will list the contents of an S3 bucket
+*/
+
 func main() {
-	fmt.Println("\nAmazon S3 Demo")
+	fmt.Println("✅ Starting S3 Demo.")
+	// creating a context, don't know why this is used
 	ctx := context.TODO()
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("ap-south-1"))
+	// Loading AWS configuration
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("❌ Encountered a problem!")
+		os.Exit(1)
 	}
-	client := s3.NewFromConfig(cfg)
-	output, err := client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
-		Bucket: aws.String("clidemonstration"),
+	// creating an S3 client; the access credentials give access only to S3
+	awsClientS3 := s3.NewFromConfig(cfg)
+	// getting bucket results using ListObjectsV2
+	output, err := awsClientS3.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
+		Bucket: aws.String(os.Getenv("DEFAULT_BUCKET_NAME")),
 	})
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("❌ Encountered a problem!")
 	}
-	if len(output.Contents) == 0 {
-		fmt.Println("Bucket empty!")
+	if output.KeyCount == 0 {
+		fmt.Println("❌ Bucket empty!")
 	}
 	for _, object := range output.Contents {
-		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+		fmt.Printf("key=%s\nsize=%d\n", aws.ToString(object.Key), object.Size)
 	}
 }
